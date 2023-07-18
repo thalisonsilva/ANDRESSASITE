f4f817ee95de10813175aad2ec6150dae62303f9
Thalison Silva <contato.thalison@outlook.com>
2023-07-18 19:06:47 -0300

    cor card - dep

--- slide.js
+++ slide.js
@@ -35,3 +35,33 @@ window.addEventListener('DOMContentLoaded', function() {
       nextButton.style.display = scrollPosition < cardContainer.scrollWidth - cardContainer.offsetWidth ? 'block' : 'none';
     }
   });
+
+  window.addEventListener('DOMContentLoaded', function() {
+    // Obtenha a referência da div onde o tour será exibido
+    var divTour = document.getElementById('tour-360');
+    
+    // Crie um elemento iframe
+    var iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = '70%';
+    iframe.src = 'https://studiodayanavlasack.mmstourvirtual360.com/';
+    iframe.allow = 'autoplay; fullscreen; gyroscope; accelerometer';
+    
+    // Adicione o iframe à div
+    divTour.appendChild(iframe);
+  });
+
+  window.addEventListener('DOMContentLoaded', function() {
+    // Obtenha a referência da div onde o tour será exibido
+    var divTour = document.getElementById('tour-360-2');
+    
+    // Crie um elemento iframe
+    var iframe = document.createElement('iframe');
+    iframe.width = '100%';
+    iframe.height = '70%';
+    iframe.src = 'https://casabenaventes.mmstourvirtual360.com/';
+    iframe.allow = 'autoplay; fullscreen; gyroscope; accelerometer';
+    
+    // Adicione o iframe à div
+    divTour.appendChild(iframe);
+  });
